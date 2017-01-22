fd1af285f1eedb10fbc0db1dda0c189abb5cb9a7
Tom Butler <tom@r.je>
2017-01-22 20:42:16 +0000

    clicking now starts a timer

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-function myClickEvent() {
+function myInterval() {
   var element = document.getElementById('circle');
-  var circleOpacity = parseFloat(element.style.opacity);
-  element.style.opacity = circleOpacity - 0.1;
+  var circleOpacity = element.style.opacity;
+
+  element.style.opacity = circleOpacity - 0.01;
+}
+
+function myClickEvent() {
+  setInterval(myInterval, 10);
 }
 
 function myLoadEvent() {
   var element = document.getElementById('circle');
   element.addEventListener('click', myClickEvent);
-  element.style.opacity = '0.5';
+  element.style.opacity = 1;
 }
 
 document.addEventListener('DOMContentLoaded', myLoadEvent);
