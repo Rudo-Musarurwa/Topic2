5f149711152d9444ac153640e10559dd3a5aea22
Tom Butler <tom@r.je>
2017-01-22 20:47:08 +0000

    revert overwrite

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-function myKeyDown() {
+function myInterval() {
   var element = document.getElementById('circle');
-  var positionLeft = element.offsetLeft;
-  
-  element.style.left = positionLeft - 10 + 'px';
+  var circleOpacity = element.style.opacity;
+
+  element.style.opacity = circleOpacity - 0.01;
+}
+
+function myClickEvent() {
+  setInterval(myInterval, 10);
 }
 
 function myLoadEvent() {
-  document.addEventListener('keydown', myKeyDown);
+  var element = document.getElementById('circle');
+  element.addEventListener('click', myClickEvent);
+  element.style.opacity = 1;
 }
 
 document.addEventListener('DOMContentLoaded', myLoadEvent);
