08ad61c150a4baac03d73805d5d8c70363a971a3
Tom Butler <tom@r.je>
2017-01-22 20:26:13 +0000

    circle goes to 1 opacity when clicked

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+function myClickEvent() {
+  var element = document.getElementById('circle');
+  element.style.opacity = '1';
+}
 
 function myLoadEvent() {
   var element = document.getElementById('circle');
+  element.addEventListener('click', myClickEvent);
   element.style.opacity = '0.5';
 }
 
