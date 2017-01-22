7d826db568f2a80cd7962033eab7aed3fab7a3de
Tom Butler <tom@r.je>
2017-01-22 20:56:51 +0000

    key press moves arrow in relevant direction

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-function myKeyDown() {
-  var element = document.getElementById('circle');
-  var positionLeft = element.offsetLeft;
-  
-  element.style.left = positionLeft - 10 + 'px';
+function myKeyDown(event) {
+	console.log(event.keyCode);
+  if (event.keyCode == 37) {
+	  var element = document.getElementById('circle');
+	  var positionLeft = element.offsetLeft;
+	  element.style.left = positionLeft - 10 + 'px';
+  }
+  if (event.keyCode == 38) {
+	  var element = document.getElementById('circle');
+	  var positionTop = element.offsetTop;
+	  element.style.top = positionTop - 10 + 'px';
+  }
+  if (event.keyCode == 40) {
+	  var element = document.getElementById('circle');
+	  var positionTop = element.offsetTop;
+	  element.style.top = positionTop + 10 + 'px';
+  }
+  if (event.keyCode == 39) {
+    var element = document.getElementById('circle');
+  	var positionLeft = element.offsetLeft;
+ 	element.style.left = positionLeft + 10 + 'px';
+  }
 }
 
 function myLoadEvent() {
